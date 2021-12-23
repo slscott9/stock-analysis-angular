83c1d64c8000241882532589d6741a4093d0917b
slscott9 <baseballdude9cf@gmail.com>
2021-12-23 12:55:30 -0600

    Set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { UserHomeComponent } from './components/home/user-home/user-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
