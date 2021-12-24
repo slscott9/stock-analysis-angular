852d99cfa0e948e2aeeb59b2a48f79c4edb0bdac
slscott9 <baseballdude9cf@gmail.com>
2021-12-23 22:06:34 -0600

    Setting up user home ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { LoginComponent } from './components/authentication/login/login.componen
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { UserHomeComponent } from './components/home/user-home/user-home.component';
 import { AuthInterceptor } from './interceptor/interceptor';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { InvestmentsComponent } from './components/investments/investments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    UserHomeComponent
+    UserHomeComponent,
+    SideBarComponent,
+    InvestmentsComponent
   ],
   imports: [
     BrowserModule,
